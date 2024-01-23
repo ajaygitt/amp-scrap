f890d142a0d6c5bfae898528309cbe3f8572c795
ajaypradeep <ajaypradeep1712@gmail.com>
2024-01-23 18:11:03 +0530

    logging added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 var express = require("express");
 var router = express.Router();
+const winston = require('winston');
+const { format } = require('date-fns');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'DD/MM/YY HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      const logObject = { timestamp, level };
+      logObject.requestBody = JSON.parse(message);
+      return JSON.stringify(logObject);
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'app.log' })
+  ],
+});
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,20 +28,17 @@ router.get("/", function (req, res, next) {
 router.post("/getResponse", (req, res) => {
   try {
     let data = req.body;
-    let resData = {
-      responseParam1: 1,
-      responseParam2: 2,
-      responseParam3: 3,
-      responseParam4: 4,
-      success: false,
-    };
-
+    
     var acceptOrderKey = Object.keys(data).find(key => key.startsWith("accept_order"));
     var acceptOrderValue = data[acceptOrderKey];
 
+    const timestamp = format(new Date(), 'dd/MM/yy HH:mm:ss');
+    const logMessage = JSON.stringify(req.body);
+    logger.info(logMessage);
+  
+
     if (acceptOrderValue=="1") {
       //counter
-      resData.success = true;
      return res.render('singlesource_after_counter')
 
     } else{
